73fb083506b4a6b45cf005ff2278699e6c8703b4
Greg Signal <greg@awesome.org.nz>
2014-10-29 13:14:57 +1300

    Extract config and remove a bunch of state passing
    Decouples StateManger, Adapters and Models a great deal now that they
    don't require state to be passed among them. Adapters can be
    instantiated as they are needed, and don't rely on instances of
    StateManager or other adapters to function.

--- src/model/node.js
+++ src/model/node.js
@@ -1,6 +1,7 @@
 var _       = require('lodash');
 var Promise = require('promise');
 
+var TrailblazerHTTPStorageAdapter = require('../adapter/trailblazer_http_storage_adapter');
 
 module.exports = Node;
 /**
@@ -63,21 +64,19 @@ Node._getId = function() {
 
 /**
  * Return a Node, if it exists, corresponding to the given ID.
- * @param {StorageAdapter} adapter - The storage adapter to query
  * @param {number} id - The ID of the {@link Node}
  * @returns {Node}
  */
-Node.cache.read = function(adapter, id) {
+Node.cache.read = function(id) {
   return Node._instances[id];
 };
 
 /**
  * Return all Nodes in the cache
- * @param {StorageAdapter} adapter - The storage adapter to query
  * @param {number} id - The ID of the {@link Node}
  * @returns {Array<Node>}
  */
-Node.cache.list = function(adapter, assignmentId) {
+Node.cache.list = function(assignmentId) {
   var conditions = {};
   if (assignmentId) conditions.assignmentId = assignmentId;
   return _.where(Node._instances, conditions);
@@ -85,23 +84,21 @@ Node.cache.list = function(adapter, assignmentId) {
 
 /**
  * Request a Node from the storage adapter, corresponding to the provided ID
- * @param {StorageAdapter} adapter - The storage adapter to query
  * @param {number} id - The ID of the {@link Node}
  * @returns {Node}
  */
-Node.read = function(adapter, id) {
-  return adapter.read("nodes", id);
+Node.read = function(id) {
+  return new TrailblazerHTTPStorageAdapter().read("nodes", id);
 };
 
 /**
  * Request all Nodes from the storage adapter.
- * @param {StorageAdapter} adapter - The storage adapter to query
  * @returns {Promise} - Resolves with Array<Node> or an error object
  */
-Node.list = function(adapter, assignmentId) {
+Node.list = function(assignmentId) {
   return new Promise(function(resolve, reject) {
     // Request assignments from the storage adapter
-    adapter.list(["assignments", assignmentId, "nodes"].join("/")).then(
+    new TrailblazerHTTPStorageAdapter().list(["assignments", assignmentId, "nodes"].join("/")).then(
       function(response) {
         // Update our cache with the response (only for keys in the response -
         // we may have unsaved models)
@@ -110,7 +107,7 @@ Node.list = function(adapter, assignmentId) {
         }.bind(this));
 
         // Resolve the promise with the assignments
-        resolve(Node.cache.list(adapter));
+        resolve(Node.cache.list());
       }.bind(this),
 
       // Reject the Promise with the adapter error
@@ -118,7 +115,7 @@ Node.list = function(adapter, assignmentId) {
   }.bind(this));
 };
 
-Node.prototype.save = function(adapter) {
+Node.prototype.save = function() {
   var updateChildren = function(parentNode) {
     var children = Node.where({
       parentId: parentNode.tempId,
@@ -136,7 +133,7 @@ Node.prototype.save = function(adapter) {
     return new Promise(function(resolve, reject) {
       if (typeof this.id === "number") {
         // It's been saved before
-        adapter.update("nodes", this.id, { node: this.toProps() }, {}).then(function(response) {
+        new TrailblazerHTTPStorageAdapter().update("nodes", this.id, { node: this.toProps() }, {}).then(function(response) {
           this.id = response.id;
           Node._instances[this.id] = this;
           updateChildren(this);
@@ -145,7 +142,7 @@ Node.prototype.save = function(adapter) {
 
       } else if (this.id.indexOf("TB_.") >= 0) {
         // It's a temporary ID
-        adapter.create("nodes", { node: this.toProps() }, {
+        new TrailblazerHTTPStorageAdapter().create("nodes", { node: this.toProps() }, {
           parentResource: {
             name: "assignments",
             id: this.assignmentId
@@ -164,10 +161,10 @@ Node.prototype.save = function(adapter) {
   }
 };
 
-Node.prototype.destroy = function(adapter) {
+Node.prototype.destroy = function() {
   if (typeof this.id === "number") {
     // It's not a temp ID, so it should be persisted on the server
-    adapter.destroy("nodes", this.id);
+    new TrailblazerHTTPStorageAdapter().destroy("nodes", this.id);
   }
   // Purge from the cache
   delete Node._instances[this.id];
