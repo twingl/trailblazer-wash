225f9ae7c73b8113c95028ac59ba487ee85a70b9
David Fuentes <david@davoscript.com>
2016-07-10 01:58:14 -0400

    Voting buttons don't need to listen the change event

--- src/components/downvote.js
+++ src/components/downvote.js
@@ -14,19 +14,19 @@ export default class Downvote extends React.Component {
     };
   }
 
-  componentDidMount() {
-    // listen for `change` evts for the current node
-    messageChannel.listen( function (message) {
-      switch (message.action) {
-        case Constants.__change__:
-          if (message.payload.store === "NodeStore" && message.payload.node.localId === this.props.node.localId) {
-            this.props.node.rank = message.payload.node.rank;
-            //this.setState({ rank: message.payload.node.rank });
-          }
-          break;
-      }
-    }.bind(this));
-  }
+//  componentDidMount() {
+//    // listen for `change` evts for the current node
+//    messageChannel.listen( function (message) {
+//      switch (message.action) {
+//        case Constants.__change__:
+//          if (message.payload.store === "NodeStore" && message.payload.node.localId === this.props.node.localId) {
+//            this.props.node.rank = message.payload.node.rank;
+//            //this.setState({ rank: message.payload.node.rank });
+//          }
+//          break;
+//      }
+//    }.bind(this));
+//  }
 
   render() {
     var width = this.props.width + "px";
--- src/components/upvote.js
+++ src/components/upvote.js
@@ -14,20 +14,20 @@ export default class Upvote extends React.Component {
     };
   }
 
-  componentDidMount() {
-    // listen for `change` evts for the current node
-    messageChannel.listen( function (message) {
-      switch (message.action) {
-        case Constants.__change__:
-          if (message.payload.store === "NodeStore" &&
-              message.payload.node.localId === this.props.node.localId) {
-            this.props.node.rank = message.payload.node.rank;
-            //this.setState({ rank: message.payload.node.rank });
-          }
-          break;
-      }
-    }.bind(this));
-  }
+//  componentDidMount() {
+//    // listen for `change` evts for the current node
+//    messageChannel.listen( function (message) {
+//      switch (message.action) {
+//        case Constants.__change__:
+//          if (message.payload.store === "NodeStore" &&
+//              message.payload.node.localId === this.props.node.localId) {
+//            this.props.node.rank = message.payload.node.rank;
+//            //this.setState({ rank: message.payload.node.rank });
+//          }
+//          break;
+//      }
+//    }.bind(this));
+//  }
 
   render() {
     var classes = this.state.rank === 1 ? "upvote button selected" : "upvote button";
